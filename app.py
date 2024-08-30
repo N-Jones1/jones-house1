cde8d5089cad99123cddacece8d563f91219f198
Nick Jones <nickjones@Nicks-MacBook-Air.local>
2024-08-29 21:38:56 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+print('Welocme to the Jones family house of horrors.')
+print('you fly from florida to see your long distance girl friend and make your way to the front door of her parents house.')
+print('as you open the door you hear a loud voice in the living room yelling about the boarder wall.')
+print('do you walk up the stairs or sneak down into the dark basement?')
+print('Please type "living_room" or "basement"')
+### prompt user for choice
+roomchoice = input('> ')
+if roomchoice == 'living_room':
+    print('You climb up the stairs where an older bearded man is sitting in a reclining chair.')
+    print('he becomes startled and alerts his small dog who jumps up and latches on to your crotch.')
+    print('YOU DIED')
+if roomchoice == 'basement':
+    print('you go down the steps into the dark basement')
+    print('you hear a girls voice yelling about how shes mad at her parents about some dumb shit')
+    print('you realize its your girlfriend and jump around the corner to suprise her')
+    print('she gets mad you didnt call to tell her you arrived and she hits you with a bat.')
+    print('YOU DIED')
+
+
